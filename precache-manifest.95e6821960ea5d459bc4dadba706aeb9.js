fc8aee26ec05054867ec75a8ef943809f316154e
Filmer Tolentino <filmer.tolentino@gmail.com>
2019-11-18 15:53:42 -0800

    Updates

---
+++ precache-manifest.95e6821960ea5d459bc4dadba706aeb9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab132f8999910f15eb6734462c395e6d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2a90807845c8af449635",
+    "url": "/monsters-rolodex/static/css/main.b82924cd.chunk.css"
+  },
+  {
+    "revision": "6df4eb5567d793ed81f5",
+    "url": "/monsters-rolodex/static/js/2.975c5e1d.chunk.js"
+  },
+  {
+    "revision": "2a90807845c8af449635",
+    "url": "/monsters-rolodex/static/js/main.99525014.chunk.js"
+  },
+  {
+    "revision": "f56f2e2c6d96b014d91f",
+    "url": "/monsters-rolodex/static/js/runtime-main.86190389.js"
+  }
+]);
